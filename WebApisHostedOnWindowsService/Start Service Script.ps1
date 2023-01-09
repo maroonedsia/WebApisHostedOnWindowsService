11a6d73635e96a6f93190623651190f0e30c9617
maroonedsia <smortazavi@hotmail.com>
2023-01-08 20:59:12 -0800

    Add PowerShell script for creating the Windows service for the executable file

---
+++ WebApisHostedOnWindowsService/Start Service Script.ps1
@@ -0,0 +1,1 @@
+New-Service -Name "Sia Web APIs" -BinaryPathName "D:\Projects\_Test\WebApisHostedOnWindowsService\WebApisHostedOnWindowsService\bin\Release\net7.0\publish\WebApisHostedOnWindowsService.exe --contentRoot D:\Projects\_Test\WebApisHostedOnWindowsService\WebApisHostedOnWindowsService\bin\Release\net7.0\publish"  -StartupType Automatic
